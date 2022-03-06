65339dee5c6107caae260ee73ea2dfa67d4e0a72
AntoineDao <antoinedao1@gmail.com>
2022-03-06 17:38:37 +0000

    feat(deploy): add deploy script

--- pollination_apps/cli/__init__.py
+++ pollination_apps/cli/__init__.py
@@ -57,3 +57,59 @@ def login(environment):
     ctx.save()
 
 
+@main.command('deploy')
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-o', '--owner', help='the owner of the app on pollination (defaults to logged in user)')
+@click.option('-n', '--name', help='the name of the app (defaults to folder name)')
+@click.option('-t', '--tag', help='the tag for this version of the app')
+@click.option(
+    '-m', '--message', help='the commit message for this version of the app', show_default=True,
+    default='The best version of this app... so far!'
+)
+@click.option(
+    '-e', '--environment', help='the pollination environment',
+    type=click.Choice(['staging', 'production']),
+    default='production'
+)
+@click.option(
+    '--public/--private', help='Indicate if the recipe or plugin should be created as '
+    'a public or a private resource. This option does not change the visibility of a '
+    'resource if it already exist.', is_flag=True, default=True
+)
+def deploy(path, owner, name, tag, message, environment, public):
+    """deploy a new application version"""
+    ctx = Context.from_file()
+
+    client = ctx.client
+    env = Environment.from_string(environment)
+    client.set_host(env.api_host)
+    user = client.get_account()
+
+    if owner is None:
+        owner = user.username
+
+    path = Path(path).absolute()
+    if name is None:
+        name = path.name
+
+    slug = slugify(name)
+
+    if tag is None:
+        tag = 'latest'
+
+    try:
+        client.update_app(owner=owner, slug=slug, public=public)
+    except:
+        client.create_app(owner, slug, public)
+
+    upload_link = client.get_upload_link(
+        owner=owner,
+        slug=slug,
+        tag=tag,
+        release_notes=message,
+    )
+
+    client.upload_app_folder(
+        link=upload_link,
+        path=path,
+    )
