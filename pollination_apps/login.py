359e3f210849cedc5e2773e0b7ab801fe976f22b
AntoineDao <antoinedao1@gmail.com>
2022-04-10 10:22:14 +0000

    fix(login): use threading http server for chrome see https://stackoverflow.com/questions/43146298/http-request-from-chrome-hangs-python-webserver

--- pollination_apps/login.py
+++ pollination_apps/login.py
@@ -1,25 +1,38 @@
+import pathlib
+import tempfile
 import webbrowser
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from urllib.parse import parse_qs, urlparse
 
 IP = "127.0.0.1"
 PORT = 8645
 
+JWT_FILE = pathlib.Path('user-jwt.txt')
+
 
 class RequestHandler(BaseHTTPRequestHandler):
+
+    jwt_file = tempfile.TemporaryFile()
 
     def do_GET(self):
         """Serve a GET request."""
         url = urlparse(self.path)
         qs = parse_qs(url.query)
         jwt = qs['token'][0]
-        self.wfile.write("Success".encode('utf-8'))
-        self.server.jwt = jwt
+        JWT_FILE.write_text(jwt)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Success")
+        self.server.shutdown()
+
+    def log_message(self, format, *args):
+        return
 
 
 def interactive_login(url: str = 'https://auth.staging.pollination.cloud/sdk-login') -> str:
-    httpd = HTTPServer((IP, PORT), RequestHandler)
+    httpd = ThreadingHTTPServer((IP, PORT), RequestHandler)
     webbrowser.open_new(url)
-    httpd.handle_request()
-    # pylint: disable=no-member
-    return httpd.jwt
+    httpd.serve_forever()
+    jwt = JWT_FILE.read_text()
+    JWT_FILE.unlink()
+    return jwt
