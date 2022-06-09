0ae0e819e443ead9ec4d2b1ee51e0fa49467dfef
Devang <devngc@gmail.com>
2022-06-09 14:24:27 -0400

    feat(post_gen_project): add support for vtk if requested

--- pollination_apps/template/assets/hooks/post_gen_project.py
+++ pollination_apps/template/assets/hooks/post_gen_project.py
@@ -1,9 +1,16 @@
+"""This module performs post-generation tasks once the project is created.
+These tasks may include: removing files, renaming files, etc.
+"""
+
 import os
 import shutil
+from pathlib import Path
 
+# Remove .github folder if CI is not requested
 REMOVE_PATHS = [
     '{% if cookiecutter.ci != "github-actions" %} .github {% endif %}',
 ]
+
 
 for path in REMOVE_PATHS:
     path = path.strip()
@@ -12,3 +19,23 @@ for path in REMOVE_PATHS:
             shutil.rmtree(path)
         else:
             os.unlink(path)
+
+
+# If Pollination_viewer is requested, add support for vtk in the Dockerfile
+NEEDS_VIEWER = '{% if cookiecutter.pollination_viewer == "yes" %} yes {% endif %}'
+if NEEDS_VIEWER.strip() == 'yes':
+    docker_file_path = Path('./app/Dockerfile')
+    with open(docker_file_path, 'r') as f:
+        docker_file_data = f.readlines()
+
+    vtk_support = [
+        'RUN apt-get update \\ \n',
+        '\t&& apt-get -y install ffmpeg libsm6 libxext6 xvfb --no-install-recommends \\ \n',
+        '\t&& apt-get clean \\ \n',
+        '\t&& rm -rf /var/lib/apt/lists/* \n',
+        '\n',
+    ]
+
+    new_docker_file_data = docker_file_data[0:2] + vtk_support + docker_file_data[2:]
+    with open(docker_file_path, 'w') as f:
+        f.writelines(new_docker_file_data)
