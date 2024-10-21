6bae0ea7e8146373cbdba85c86303347531a84c6
antoinedao <antoinedao1@gmail.com>
2024-10-21 08:09:17 +0100

    fix(domains): point template values to new domains

--- pollination_apps/template/dash/{{cookiecutter.app_name.lower().replace(' ', '-') }}/app/app.py
+++ pollination_apps/template/dash/{{cookiecutter.app_name.lower().replace(' ', '-') }}/app/app.py
@@ -1,11 +1,13 @@
 import os
-import pollination_dash_io
+
 import dash
+import dash_renderjson
+import pollination_dash_io
 from dash import html
 from dash.dependencies import Input, Output
-import dash_renderjson
 
-base_path = os.getenv('POLLINATION_API_URL', 'https://api.pollination.cloud')
+base_path = os.getenv('POLLINATION_API_URL',
+                      'https://api.pollination.solutions')
 
 app = dash.Dash(__name__)
 server = app.server
@@ -23,17 +25,20 @@ app.layout = html.Div([
     html.Div(id='output-form')
 ])
 
-api_key.create_api_key_callback(app=app, component_ids=['auth-user', 'po-select-project'])
+api_key.create_api_key_callback(
+    app=app, component_ids=['auth-user', 'po-select-project'])
+
 
 @app.callback(
     Output(component_id='output-form',
-      component_property='children'),
+           component_property='children'),
     Input(component_id='po-select-project',
-      component_property='project')
+          component_property='project')
 )
 def get_value_from_recipe_form(data):
     return dash_renderjson.DashRenderjson(id='json-out',
-                                          data={ 'project': data })
+                                          data={'project': data})
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
